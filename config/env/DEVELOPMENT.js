e216155d9fea6301a19c2e1f064acc7079cfb350
giriganapathy <giriganapathy@gmail.com>
2017-03-16 18:13:31 +0530

    config-env-commit 3

--- config/env/DEVELOPMENT.js
+++ config/env/DEVELOPMENT.js
@@ -1,7 +1,4 @@
 //DEVELOPMENT.js
 module.exports = {
-	"session-secret": "girig",
-	"tc-url": "api-verizon-dev.touchcommerce.com",
-	"tc-user-id": "vz-wireline-reporting-api-nonprod",
-	"tc-user-pwd": "Touch2016"
+	
 }
